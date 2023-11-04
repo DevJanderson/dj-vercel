a91d487be4033bf9c584f96184c3fdce0faa3c76
Janderson Lira <devjanderlira@gmail.com>
2023-11-04 16:28:40 -0400

    corrigindo arquiv de urls

--- core/urls.py
+++ core/urls.py
@@ -7,12 +7,12 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
+
 #----------------------------------------------------
 
 
 urlpatterns = [
-    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+   
     path('admin/', admin.site.urls),
     path('', include('pages.urls')), # urls do app.pages
 
