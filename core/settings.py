37b6285379d540fd8c91bc5595df2ee95f327eaf
Janderson Lira <devjanderlira@gmail.com>
2023-11-04 15:41:25 -0400

    paste staticfiles_dirs

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'STATICFILES_DIRS', 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # used to specify additional directories from which to load static files
 
