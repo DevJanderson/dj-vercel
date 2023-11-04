1c0f4ac292ad966dc953832e6bd43481fd1970de
Janderson Lira <devjanderlira@gmail.com>
2023-11-04 15:54:25 -0400

    teste urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for core project.
 """
-from django.urls import re_path
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
@@ -12,7 +12,8 @@ from django.views.static import serve
 
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')), # urls do app.pages
 
